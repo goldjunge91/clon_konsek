5b785355510268ae4a502ba0e0ece2be3ef750f1
goldjunge91 <m.tozzi91@icloud.com>
2024-05-05 19:36:09 +0200

    test snippeds auskomemntiert zum starten der Website

--- src/lib/MISC/encryption.js
+++ src/lib/MISC/encryption.js
@@ -1,62 +1,63 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.encryptCBC = exports.encryptECB = void 0;
-var dotenv = require('dotenv');
-var cryptovar = require('crypto-js');
-// Load environment variables
-dotenv.config();
-// Get the encryption key and IV from the environment variables
-var key = process.env.ENCRYPTION_KEY || '';
-var iv = process.env.ENCRYPTION_IV || '';
-// Test values
-var originalDsmPassword = 'menwyg--ponwoX';
-var originalZipPassword = 'menwyg--ponwoX';
-// Encryption function
-var encryptPassword = function (password) {
-    var parsedKey = cryptovar.enc.Utf8.parse(key);
-    var parsedIV = cryptovar.enc.Utf8.parse(iv);
-    return cryptovar.AES.encrypt(password, parsedKey, {
-        iv: parsedIV,
-        mode: cryptovar.mode.CBC,
-        padding: cryptovar.pad.Pkcs7,
-    }).toString();
-};
-// Encrypt passwords
-var encryptedDsmPassword = encryptPassword(originalDsmPassword);
-var encryptedZipPassword = encryptPassword(originalZipPassword);
-console.log('Verschlüsselte Passwörter:');
-console.log('DSM Password:', encryptedDsmPassword);
-console.log('Zip Password:', encryptedZipPassword);
-// import all necessary modules from crypto-js
-var crypto_js_1 = require("crypto-js");
-// initialize dotenv
-dotenv.config();
-var key = crypto_js_1.enc.Utf8.parse(process.env.ENCRYPTION_KEY);
-var iv = crypto_js_1.enc.Utf8.parse(process.env.ENCRYPTION_IV); // Nur nötig für CBC
-// export function encryptECB(message: string): string {
-//   return AES.encrypt(message, key, { mode: mode.ECB, padding: pad.Pkcs7 }).toString();
+// /* eslint-disable @typescript-eslint/no-var-requires */
+// "use strict";
+// Object.defineProperty(exports, "__esModule", { value: true });
+// exports.encryptCBC = exports.encryptECB = void 0;
+// var dotenv = require('dotenv');
+// var cryptovar = require('crypto-js');
+// // Load environment variables
+// dotenv.config();
+// // Get the encryption key and IV from the environment variables
+// var key = process.env.ENCRYPTION_KEY || '';
+// var iv = process.env.ENCRYPTION_IV || '';
+// // Test values
+// var originalDsmPassword = 'menwyg--ponwoX';
+// var originalZipPassword = 'menwyg--ponwoX';
+// // Encryption function
+// var encryptPassword = function (password) {
+//     var parsedKey = cryptovar.enc.Utf8.parse(key);
+//     var parsedIV = cryptovar.enc.Utf8.parse(iv);
+//     return cryptovar.AES.encrypt(password, parsedKey, {
+//         iv: parsedIV,
+//         mode: cryptovar.mode.CBC,
+//         padding: cryptovar.pad.Pkcs7,
+//     }).toString();
+// };
+// // Encrypt passwords
+// var encryptedDsmPassword = encryptPassword(originalDsmPassword);
+// var encryptedZipPassword = encryptPassword(originalZipPassword);
+// console.log('Verschlüsselte Passwörter:');
+// console.log('DSM Password:', encryptedDsmPassword);
+// console.log('Zip Password:', encryptedZipPassword);
+// // import all necessary modules from crypto-js
+// var crypto_js_1 = require("crypto-js");
+// // initialize dotenv
+// dotenv.config();
+// var key = crypto_js_1.enc.Utf8.parse(process.env.ENCRYPTION_KEY);
+// var iv = crypto_js_1.enc.Utf8.parse(process.env.ENCRYPTION_IV); // Nur nötig für CBC
+// // export function encryptECB(message: string): string {
+// //   return AES.encrypt(message, key, { mode: mode.ECB, padding: pad.Pkcs7 }).toString();
+// // }
+// // export function encryptCBC(message: string): string {
+// //   return AES.encrypt(message, key, { iv, mode: mode.CBC, padding: pad.Pkcs7 }).toString();
+// // }
+// // Nur nötig für CBC
+// function encryptECB(message) {
+//     return crypto_js_1.enc.Base64.stringify(crypto_js_1.AES.encrypt(message, key, {
+//         mode: crypto_js_1.mode.ECB,
+//         padding: crypto_js_1.pad.Pkcs7
+//     }).ciphertext);
 // }
-// export function encryptCBC(message: string): string {
-//   return AES.encrypt(message, key, { iv, mode: mode.CBC, padding: pad.Pkcs7 }).toString();
+// exports.encryptECB = encryptECB;
+// function encryptCBC(message) {
+//     return crypto_js_1.enc.Base64.stringify(crypto_js_1.AES.encrypt(message, key, {
+//         iv: iv,
+//         mode: crypto_js_1.mode.CBC,
+//         padding: crypto_js_1.pad.Pkcs7
+//     }).ciphertext);
 // }
-// Nur nötig für CBC
-function encryptECB(message) {
-    return crypto_js_1.enc.Base64.stringify(crypto_js_1.AES.encrypt(message, key, {
-        mode: crypto_js_1.mode.ECB,
-        padding: crypto_js_1.pad.Pkcs7
-    }).ciphertext);
-}
-exports.encryptECB = encryptECB;
-function encryptCBC(message) {
-    return crypto_js_1.enc.Base64.stringify(crypto_js_1.AES.encrypt(message, key, {
-        iv: iv,
-        mode: crypto_js_1.mode.CBC,
-        padding: crypto_js_1.pad.Pkcs7
-    }).ciphertext);
-}
-exports.encryptCBC = encryptCBC;
-// Beispielnutzung
-var encryptedECB = encryptECB("I love Medium");
-var encryptedCBC = encryptCBC("I love Medium");
-console.log("encrypted ECB: ", encryptedECB);
-console.log("encrypted CBC: ", encryptedCBC);
+// exports.encryptCBC = encryptCBC;
+// // Beispielnutzung
+// var encryptedECB = encryptECB("I love Medium");
+// var encryptedCBC = encryptCBC("I love Medium");
+// console.log("encrypted ECB: ", encryptedECB);
+// console.log("encrypted CBC: ", encryptedCBC);
--- src/lib/MISC/encryption.ts
+++ src/lib/MISC/encryption.ts
@@ -1,74 +1,75 @@
-const dotenv = require('dotenv');
-const cryptovar = require('crypto-js');
+// /* eslint-disable @typescript-eslint/no-var-requires */
+// const dotenv = require('dotenv');
+// const cryptovar = require('crypto-js');
 
-// Load environment variables
-dotenv.config();
+// // Load environment variables
+// dotenv.config();
 
-// Get the encryption key and IV from the environment variables
-const key = process.env.ENCRYPTION_KEY || '';
-const iv = process.env.ENCRYPTION_IV || '';
+// // Get the encryption key and IV from the environment variables
+// const key = process.env.ENCRYPTION_KEY || '';
+// const iv = process.env.ENCRYPTION_IV || '';
 
-// Test values
-const originalDsmPassword = 'menwyg--ponwoX';
-const originalZipPassword = 'menwyg--ponwoX';
+// // Test values
+// const originalDsmPassword = 'menwyg--ponwoX';
+// const originalZipPassword = 'menwyg--ponwoX';
 
-// Encryption function
-const encryptPassword = (password: string) => {
-  const parsedKey = cryptovar.enc.Utf8.parse(key);
-  const parsedIV = cryptovar.enc.Utf8.parse(iv);
-  return cryptovar.AES.encrypt(password, parsedKey, {
-    iv: parsedIV,
-    mode: cryptovar.mode.CBC,
-    padding: cryptovar.pad.Pkcs7,
-  }).toString();
-};
+// // Encryption function
+// const encryptPassword = (password: string) => {
+//   const parsedKey = cryptovar.enc.Utf8.parse(key);
+//   const parsedIV = cryptovar.enc.Utf8.parse(iv);
+//   return cryptovar.AES.encrypt(password, parsedKey, {
+//     iv: parsedIV,
+//     mode: cryptovar.mode.CBC,
+//     padding: cryptovar.pad.Pkcs7,
+//   }).toString();
+// };
 
-// Encrypt passwords
-const encryptedDsmPassword = encryptPassword(originalDsmPassword);
-const encryptedZipPassword = encryptPassword(originalZipPassword);
+// // Encrypt passwords
+// const encryptedDsmPassword = encryptPassword(originalDsmPassword);
+// const encryptedZipPassword = encryptPassword(originalZipPassword);
 
-console.log('Verschlüsselte Passwörter:');
-console.log('DSM Password:', encryptedDsmPassword);
-console.log('Zip Password:', encryptedZipPassword);
+// console.log('Verschlüsselte Passwörter:');
+// console.log('DSM Password:', encryptedDsmPassword);
+// console.log('Zip Password:', encryptedZipPassword);
 
 
-// import all necessary modules from crypto-js
-import { AES, mode, pad, enc } from 'crypto-js';
-import dotenv from 'dotenv';
+// // import all necessary modules from crypto-js
+// import { AES, mode, pad, enc } from 'crypto-js';
+// import dotenv from 'dotenv';
 
-// initialize dotenv
-dotenv.config();
+// // initialize dotenv
+// dotenv.config();
 
-const key = enc.Utf8.parse(process.env.ENCRYPTION_KEY!);
-const iv = enc.Utf8.parse(process.env.ENCRYPTION_IV!); // Nur nötig für CBC
+// const key = enc.Utf8.parse(process.env.ENCRYPTION_KEY!);
+// const iv = enc.Utf8.parse(process.env.ENCRYPTION_IV!); // Nur nötig für CBC
 
+// // export function encryptECB(message: string): string {
+// //   return AES.encrypt(message, key, { mode: mode.ECB, padding: pad.Pkcs7 }).toString();
+// // }
+
+// // export function encryptCBC(message: string): string {
+// //   return AES.encrypt(message, key, { iv, mode: mode.CBC, padding: pad.Pkcs7 }).toString();
+// // }
+
+// // Nur nötig für CBC
 // export function encryptECB(message: string): string {
-//   return AES.encrypt(message, key, { mode: mode.ECB, padding: pad.Pkcs7 }).toString();
+//   return enc.Base64.stringify(AES.encrypt(message, key, { 
+//     mode: mode.ECB, 
+//     padding: pad.Pkcs7 
+//   }).ciphertext);
 // }
 
 // export function encryptCBC(message: string): string {
-//   return AES.encrypt(message, key, { iv, mode: mode.CBC, padding: pad.Pkcs7 }).toString();
+//   return enc.Base64.stringify(AES.encrypt(message, key, {
+//     iv,
+//     mode: mode.CBC,
+//     padding: pad.Pkcs7
+//   }).ciphertext);
 // }
 
-// Nur nötig für CBC
-export function encryptECB(message: string): string {
-  return enc.Base64.stringify(AES.encrypt(message, key, { 
-    mode: mode.ECB, 
-    padding: pad.Pkcs7 
-  }).ciphertext);
-}
+// // Beispielnutzung
+// const encryptedECB = encryptECB("I love Medium");
+// const encryptedCBC = encryptCBC("I love Medium");
 
-export function encryptCBC(message: string): string {
-  return enc.Base64.stringify(AES.encrypt(message, key, {
-    iv,
-    mode: mode.CBC,
-    padding: pad.Pkcs7
-  }).ciphertext);
-}
-
-// Beispielnutzung
-const encryptedECB = encryptECB("I love Medium");
-const encryptedCBC = encryptCBC("I love Medium");
-
-console.log("encrypted ECB: ", encryptedECB);
-console.log("encrypted CBC: ", encryptedCBC);
+// console.log("encrypted ECB: ", encryptedECB);
+// console.log("encrypted CBC: ", encryptedCBC);
