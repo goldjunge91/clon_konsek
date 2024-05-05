1bf564d104f5615b61a6e64c2d69a696544dde7b
goldjunge91 <m.tozzi91@icloud.com>
2024-05-05 19:22:28 +0200

    refactor: Add cron job to schedule folder deletion

--- src/lib/db_utils.py
+++ src/lib/db_utils.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from crontab import CronTab
+from datetime import datetime, timedelta
 
 # Laden der Umgebungsvariablen aus der .env-Datei
 load_dotenv(os.path.join(os.path.dirname(__file__), '../../.env'))
@@ -12,7 +14,9 @@ DB_NAME = os.environ.get('DB_NAME', 'postgres')
 DB_USER = os.environ.get('DB_USER', 'postgres')
 DB_PASSWORD = os.environ.get('DB_PASSWORD', 'Iphone5S')
 
-def update_task_status_in_db(task_id: str, new_status: str) -> None:
+base_path = "/home/marco/git/pdf-website/DATA/downloads/"
+
+def update_task_status_in_db(task_id: str, new_status: str) -> bool:
     try:
         conn = psycopg2.connect(
             host=DB_HOST,
@@ -20,23 +24,35 @@ def update_task_status_in_db(task_id: str, new_status: str) -> None:
             user=DB_USER,
             password=DB_PASSWORD
         )
-
         cursor = conn.cursor(cursor_factory=RealDictCursor)
         update_query = "UPDATE task SET status = %s WHERE id = %s"
         cursor.execute(update_query, (new_status, task_id))
         conn.commit()
 
         updated_rows = cursor.rowcount
-
-        if updated_rows > 0:
-            return
-            # print(f"Task mit ID {task_id} wurde auf '{new_status}' gesetzt.")
-        else:
-            return
-            # print(f"Task mit ID {task_id} konnte nicht aktualisiert werden.")
-
         cursor.close()
         conn.close()
 
+        return updated_rows > 0
+
     except (Exception, psycopg2.Error) as error:
-        print(f"Fehler beim Aktualisieren des Task-Status: {error}")
+        logging.error(f"Fehler beim Aktualisieren des Task-Status: {error}")
+        return False
+
+def schedule_delete_folder_cronjob(folder_path: str) -> None:
+    try:
+        # Create a new cron job
+        cron = CronTab(user=True)
+        command = f"rm -rf {folder_path}"
+        job = cron.new(command=command)
+
+        # Set the job to run in 2 hours
+        job.setall("0 */2 * * *")
+
+        # Write the cron job to the crontab file
+        cron.write()
+
+        print(f"Cronjob scheduled to delete folder: {folder_path}")
+
+    except Exception as error:
+        print(f"Fehler beim Planen des Cronjobs: {error}")
