0e54524625b6df5b098531478d15b7ddc7b5afbd
goldjunge91 <m.tozzi91@icloud.com>
2024-05-13 14:47:17 +0200

    refactor: Update script command to use 'npx next start' instead of 'npm run start -d'

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,37 +2,14 @@
 module.exports = {
   apps: [
       {
-          script: 'npm run start -d',
+          script: 'npx next start',
           instances: 1,
           name: 'website',
-          log_date_format: 'YYYY-MM-DD HH:mm:ss.SSS',
           error_file: '/home/marco/git/logs/error.log',
           out_file: '/home/marco/git/logs/out.log',
-          combine_logs: true,
-          merge_logs: true,
+          log_date_format: 'YYYY-MM-DD HH:mm:ss.SSS',
+          // combine_logs: true,
+          // merge_logs: true,
       },
   ],
 }
-
-// module.exports = {
-//   apps : [{
-//     script: 'index.js',
-//     watch: '.'
-//   }, {
-//     script: './service-worker/',
-//     watch: ['./service-worker']
-//   }],
-
-//   deploy : {
-//     production : {
-//       user : 'SSH_USERNAME',
-//       host : 'SSH_HOSTMACHINE',
-//       ref  : 'origin/master',
-//       repo : 'GIT_REPOSITORY',
-//       path : 'DESTINATION_PATH',
-//       'pre-deploy-local': '',
-//       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-//       'pre-setup': ''
-//     }
-//   }
-// };
