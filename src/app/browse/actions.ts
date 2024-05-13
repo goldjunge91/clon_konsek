790ee98af8d4795ffa7abc68a8f512e7c6b57003
goldjunge91 <m.tozzi91@icloud.com>
2024-05-13 14:54:20 +0200

    refactor: Add deleteAsAdminTask function for deleting tasks as an admin

---
+++ src/app/browse/actions.ts
@@ -0,0 +1,43 @@
+/* eslint-disable */
+
+"use server";
+
+import { deleteTask, getTask } from "@/data-access/tasks";
+
+import { getSession } from "@/app/api/auth/[...nextauth]/options";
+import { revalidatePath } from "next/cache";
+// import NextAuth from "next-auth/next";
+import { useSession } from "next-auth/react";
+
+export async function deleteAsAdminTask(taskId: string) {
+  const session = await getSession();
+  const session_admin = useSession();
+
+  if (!session) {
+    throw new Error("User not authenticated");
+  }
+
+  const isAdmin = session_admin.data?.user?.role === "admin";
+  const task = await getTask(taskId);
+
+  if (task?.userId !== session.user.id && !isAdmin) {
+    throw new Error("User not authorized");
+  }
+
+  await deleteTask(taskId);
+  revalidatePath("/browse");
+}
+
+export async function getServerSideProps() {
+  const session = await getSession();
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/signin?callbackUrl=/protected-page",
+        permanent: false,
+      },
+    };
+  }
+  // Additional checks for roles or permissions can also be performed here
+  return { props: { session } };
+}
