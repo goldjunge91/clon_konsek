08f46f2c088d4844593fd72843c378d4a93e995b
goldjunge91 <m.tozzi91@icloud.com>
2024-05-13 14:53:38 +0200

    refactor: Remove submit-button component

--- src/components/submit-button.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-"use client";
-
-import { useFormStatus } from "react-dom";
-
-export const SubmitButton = () => {
-  const { pending } = useFormStatus();
-  return <button>{pending ? "Creating Todo..." : "Create Todo"}</button>;
-};
