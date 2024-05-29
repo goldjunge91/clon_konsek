0fb0dab887c199ebd0fcd6b6268916ef1dc28276
Goldjunge91 <m.tozzi91@icloud.com>
2024-05-29 12:08:42 +0200

    reformate code.

--- src/lib/AuthProvider.tsx
+++ src/lib/AuthProvider.tsx
@@ -1,22 +1,22 @@
 "use client";
 
-import { ThemeProvider } from "@/components/theme-provider";
-import { SessionProvider } from "next-auth/react";
+import {ThemeProvider} from "@/components/theme-provider";
+import {SessionProvider} from "next-auth/react";
 
 
-export default function AuthProvider({ children }: {
+export default function AuthProvider({children}: {
     children: React.ReactNode
 }) {
     return (
         <SessionProvider>
-        <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-        >
-        {children}
-        </ThemeProvider>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+            </ThemeProvider>
         </SessionProvider>
     )
 }
