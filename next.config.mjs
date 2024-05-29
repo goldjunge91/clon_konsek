dc057d78a0146f9a8e880c8b347f6152e0bc5cc8
infwzzz682 <wzz682@haw-hamburg.de>
2024-05-29 21:04:34 +0200

    Update tailwind.config.ts next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,49 +1,23 @@
 /** @type {{cacheMaxMemorySize: number, images: {remotePatterns: [{protocol: string, hostname: string},{protocol: string, hostname: string},{protocol: string, hostname: string, port: string, pathname: string},{protocol: string, hostname: string, port: string, pathname: string}], formats: string[], unoptimized: string}, webpack: (function(*, {isServer: *}): *), reactStrictMode: boolean}} */
 const nextConfig = {
-    cacheMaxMemorySize: 0,
-    reactStrictMode: true,
-
-    // devIndicators: {
-    //   buildActivityPosition: 'bottom-right',
-    // },
-    // swcMinify: true,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'avatars.googleusercontent.com',
-            },
-            {
-                protocol: 'http',
-                hostname: 'lh3.googleusercontent.com',
-                port: '',
-                pathname: '**',
-            },
-            {
-                protocol: 'http',
-                hostname: 'lh3.googleusercontent.com',
-                port: '',
-                pathname: '/u/**',
-            },
-        ],
-        unoptimized: false,
-    },
-    webpack: (config, {isServer}) => {
+    cacheMaxMemorySize: 0, reactStrictMode: true, swcMinify: true, images: {
+        remotePatterns: [{
+            protocol: 'https', hostname: '**',
+        }, {
+            protocol: 'https', hostname: 'avatars.googleusercontent.com',
+        }, {
+            protocol: 'http', hostname: 'lh3.googleusercontent.com', port: '', pathname: '**',
+        }, {
+            protocol: 'http', hostname: 'lh3.googleusercontent.com', port: '', pathname: '/u/**',
+        },], formats: ["image/avif", "image/webp"], unoptimized: false,
+    }, webpack: (config, {isServer}) => {
         config.module.rules.push({
             // https://www.npmjs.com/package/@svgr/webpack
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
+            test: /\.svg$/, use: ['@svgr/webpack'],
         });
         if (!isServer) {
             config.resolve.fallback = {
-                ...config.resolve.fallback,
-                fs: false,
-                path: false,
-                os: false,
+                ...config.resolve.fallback, fs: false, path: false, os: false,
             };
         }
         return config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,110 +1,96 @@
 /* tslint:disable */
-import type {Config} from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
-/** @type {import('tailwindcss').Config} */
-
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 // Importieren Sie das Hintergrundbild aus dem öffentlichen Verzeichnis
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 
-const config =
-    {
-        // mode: "jit",
-        darkMode: ["class"],
-        content: [
-            "./pages/**/*.{ts,tsx}",
-            "./components/**/*{js,ts,jsx,tsx,mdx}",
-            "./app/**/*.{js,ts,jsx,tsx,mdx}",
-            "./src/**/*.{js,ts,jsx,tsx,mdx}",
-            "../../public",
-
-        ],
-        prefix: "",
-        theme: {
-            container: {
-                center: true,
-                padding: "2rem",
-                /**
-                 * screens: {
-                 *         "2xl": "1400px",
-                 *       },
-                 */
-                screens: {
-                    sm: '480px',
-                    md: '768px',
-                    lg: '976px',
-                    xl: '1440px',
-                    '2xl': "1400px",
-                },
-            },
-            backroundImage: {
-                custom: "url('../public/backround.jpg')",
-            },
-            extend: {
-                colors: {
-                    border: "hsl(var(--border))",
-                    input: "hsl(var(--input))",
-                    ring: "hsl(var(--ring))",
-                    foreground: "hsl(var(--foreground))",
-                    // backround: "backround",
-                    background: "hsl(var(--background))",
-                    primary: {
-                        DEFAULT: "hsl(var(--primary))",
-                        foreground: "hsl(var(--primary-foreground))",
-                    },
-                    secondary: {
-                        DEFAULT: "hsl(var(--secondary))",
-                        foreground: "hsl(var(--secondary-foreground))",
-                    },
-                    destructive: {
-                        DEFAULT: "hsl(var(--destructive))",
-                        foreground: "hsl(var(--destructive-foreground))",
-                    },
-                    muted: {
-                        DEFAULT: "hsl(var(--muted))",
-                        foreground: "hsl(var(--muted-foreground))",
-                    },
-                    accent: {
-                        DEFAULT: "hsl(var(--accent))",
-                        foreground: "hsl(var(--accent-foreground))",
-                    },
-                    popover: {
-                        DEFAULT: "hsl(var(--popover))",
-                        foreground: "hsl(var(--popover-foreground))",
-                    },
-                    card: {
-                        DEFAULT: "hsl(var(--card))",
-                        foreground: "hsl(var(--card-foreground))",
-                    },
-                },
-                borderRadius: {
-                    lg: "var(--radius)",
-                    md: "calc(var(--radius) - 2px)",
-                    sm: "calc(var(--radius) - 4px)",
-                },
-                keyframes: {
-                    "accordion-down": {
-                        from: {height: "0"},
-                        to: {height: "var(--radix-accordion-content-height)"},
-                    },
-                    "accordion-up": {
-                        from: {height: "var(--radix-accordion-content-height)"},
-                        to: {height: "0"},
-                    },
-                },
-                animation: {
-                    "accordion-down": "accordion-down 0.2s ease-out",
-                    "accordion-up": "accordion-up 0.2s ease-out",
-                },
-            },
+const config = {
+  // mode: "jit",
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../public",
+    "../public",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem" /**
+       * screens: {
+       *         "2xl": "1400px",
+       *       },
+       */,
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+        xxl: "1400px",
+      },
+    },
+    backroundImage: {
+      custom: "url('../public/backround.jpg')",
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        foreground: "hsl(var(--foreground))", // backround: "backround",
+        background: "hsl(var(--background))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
-        plugins: [
-            require("tailwindcss-animate"),
-            require('postcss-import'),
-            require('tailwindcss'),
-        ]
-
-    } satisfies Config;
-
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
 export default config;
