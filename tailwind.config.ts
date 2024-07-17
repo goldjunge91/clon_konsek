6901c4ee6bf5d4efecfd2e86dbe55c9d6fe98884
goldjunge91 <m.tozzi91@icloud.com>
2024-07-17 04:30:22 +0200

    update build process

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,104 +1,104 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../public",
-    "../public",
-    "./styles/**/*.css",
-  ],
-  prefix: "",
-  theme: {
-    container: {
-      padding: {
-        "2rem": "2rem",
-        'logo-padding': '8rem',
-      },
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
+		'../../public',
+		'../public',
+		'./styles/**/*.css',
+	],
+	prefix: '',
+	theme: {
+		container: {
+			padding: {
+				'2rem': '2rem',
+				'logo-padding': '8rem',
+			},
 
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      fontFamily: {
-        helvetica200: ["helvetica200", "sans-serif"],
-        helvetica500: ["helvetica500", "sans-serif"],
-        roboto300: ["roboto300", "sans-serif"],
-        roboto500: ["roboto500", "sans-serif"],
-        custom: [
-          "custom-roboto-slab-light",
-          "roboto-slab-light_normal_300",
-          "roboto-slab-medium",
-          "sans-roboto-slab-light_normal_300",
-          "YourCustomFont3",
-          "helveticaneuelt-std-thin_normal_250",
-        ],
-      },
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "#FFFFFF",
-        foreground: "#000000",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-        "text-primary": "rgb(var(--text-primary) / <alpha-value>)",
-        "text-secondary": "rgb(var(--text-secondary) / <alpha-value>)",
-        "button-bg": "rgb(var(--button-bg) / <alpha-value>)",
-        "button-text": "rgb(var(--button-text) / <alpha-value>)",
-        "button-border": "rgb(var(--button-border) / <alpha-value>)",
-        "form-background": "hsl(var(--form-background))",
-      },
-    },
-    borderRadius: {
-      lg: "var(--radius)",
-      md: "calc(var(--radius) - 2px)",
-      sm: "calc(var(--radius) - 4px)",
-    },
-    keyframes: {
-      "accordion-down": {
-        from: { height: "0" },
-        to: { height: "var(--radix-accordion-content-height)" },
-      },
-      "accordion-up": {
-        from: { height: "var(--radix-accordion-content-height)" },
-        to: { height: "0" },
-      },
-    },
-    animation: {
-      "accordion-down": "accordion-down 0.2s ease-out",
-      "accordion-up": "accordion-up 0.2s ease-out",
-    },
-  },
-  plugins: [require("tailwindcss-animate")],
+			screens: {
+				'2xl': '1400px',
+			},
+		},
+		extend: {
+			fontFamily: {
+				helvetica200: ['helvetica200', 'sans-serif'],
+				helvetica500: ['helvetica500', 'sans-serif'],
+				roboto300: ['roboto300', 'sans-serif'],
+				roboto500: ['roboto500', 'sans-serif'],
+				custom: [
+					'custom-roboto-slab-light',
+					'roboto-slab-light_normal_300',
+					'roboto-slab-medium',
+					'sans-roboto-slab-light_normal_300',
+					'YourCustomFont3',
+					'helveticaneuelt-std-thin_normal_250',
+				],
+			},
+			colors: {
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: '#FFFFFF',
+				foreground: '#000000',
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))',
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))',
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))',
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))',
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))',
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))',
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))',
+				},
+				'text-primary': 'rgb(var(--text-primary) / <alpha-value>)',
+				'text-secondary': 'rgb(var(--text-secondary) / <alpha-value>)',
+				'button-bg': 'rgb(var(--button-bg) / <alpha-value>)',
+				'button-text': 'rgb(var(--button-text) / <alpha-value>)',
+				'button-border': 'rgb(var(--button-border) / <alpha-value>)',
+				'form-background': 'hsl(var(--form-background))',
+			},
+		},
+		borderRadius: {
+			lg: 'var(--radius)',
+			md: 'calc(var(--radius) - 2px)',
+			sm: 'calc(var(--radius) - 4px)',
+		},
+		keyframes: {
+			'accordion-down': {
+				from: { height: '0' },
+				to: { height: 'var(--radix-accordion-content-height)' },
+			},
+			'accordion-up': {
+				from: { height: 'var(--radix-accordion-content-height)' },
+				to: { height: '0' },
+			},
+		},
+		animation: {
+			'accordion-down': 'accordion-down 0.2s ease-out',
+			'accordion-up': 'accordion-up 0.2s ease-out',
+		},
+	},
+	plugins: [require('tailwindcss-animate')],
 };
 export default config;
