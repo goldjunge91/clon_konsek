6f735e30ae89f7b5230af9b5980158955e7c75f2
goldjunge91 <m.tozzi91@icloud.com>
2024-07-19 17:52:40 +0200

    update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'pdf_konsek',
-			script: 'npm run pod',
+			script: 'npm run start',
 			instances: 1,
 			error_file: '/home/runneruser/logfiles/error.log',
 			out_file: '/home/runneruser/logfiles/out.log',
@@ -19,21 +19,7 @@ module.exports = {
 				followSymlinks: false,
 				usePolling: true,
 			},
-			// env_production: {
-			// 	// NODE_ENV: 'production',
-			// 	GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-			// 	GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-			// 	ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
-			// 	ENCRYPTION_IV: process.env.ENCRYPTION_IV,
-			// 	NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-			// 	NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-			// 	NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-			// 	POSTGRES_USER: process.env.POSTGRES_USER,
-			// 	POSTGRES_DB: process.env.POSTGRES_DB,
-			// 	POSTGRES_PW: process.env.POSTGRES_PW,
-			// 	ENABLE_GOOGLE_AUTH: process.env.ENABLE_GOOGLE_AUTH,
-			// 	DATABASE_URL: `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PW}@localhost:5432/${process.env.POSTGRES_DB}`,
-		},
+
 		},
 	],
 };
