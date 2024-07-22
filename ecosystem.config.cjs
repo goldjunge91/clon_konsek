84f0e09bf32a01d302a42c6203298e5a9d731c70
goldjunge91 <m.tozzi91@icloud.com>
2024-07-22 10:57:54 +0200

    pm2 log ordner pfad korriegiert

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       		args: '-c "npm run build && npm run start"',
 			instances: 1,
 			error_file: './logs/error.log',
-			out_file: './logs/logfiles/out.log',
+			out_file: './logs/out.log',
 			log_date_format: 'YYYY-MM-DD HH:mm.SSS',
 			autorestart: true,
 			watch: false,
